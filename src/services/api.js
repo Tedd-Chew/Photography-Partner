19143214842ea505698a5112f92db2058edac40b
Tedd-Chew <zou1218yu@163.com>
2026-06-10 21:09:00 +0800

    fix: api.js 改用 system.request.upload 原生文件上传 + manifest 加 system.request

--- src/services/api.js
+++ src/services/api.js
@@ -1,87 +1,70 @@
 import fetch from '@system.fetch'
-import { compressImage, toBase64 } from '../helper/image'
+import request from '@system.request'
 import { API_BASE_URL } from '../config'
 
 const BASE_URL = API_BASE_URL
 
-function request(method, path, body) {
-  return new Promise((resolve, reject) => {
+function jsonRequest(method, path, body) {
+  return new Promise(function (resolve, reject) {
     fetch.fetch({
       url: BASE_URL + path,
       method: method,
       header: { 'Content-Type': 'application/json' },
       data: body ? JSON.stringify(body) : '',
       responseType: 'json',
-      success: (res) => {
-        const d = res.data
+      success: function (res) {
+        var d = res.data
         if (d && d.ok) {
           resolve(d.data)
         } else {
           reject({ error: (d && d.error) || '请求失败' })
         }
       },
-      fail: (err, code) => reject({ err, code })
-    })
-  })
-}
-
-async function upload(path, filePath, extra = {}) {
-  const compressedUri = await compressImage(filePath, 1024, 0.8)
-  const base64 = await toBase64(compressedUri)
-
-  const boundary = '----PhotographyPartner' + Date.now()
-  const bodyParts = []
-  bodyParts.push('--' + boundary)
-  bodyParts.push('Content-Disposition: form-data; name="image"; filename="photo.jpg"')
-  bodyParts.push('Content-Type: image/jpeg')
-  bodyParts.push('')
-  bodyParts.push(base64)
-
-  const keys = Object.keys(extra)
-  for (let i = 0; i < keys.length; i++) {
-    bodyParts.push('--' + boundary)
-    bodyParts.push('Content-Disposition: form-data; name="' + keys[i] + '"')
-    bodyParts.push('')
-    bodyParts.push(String(extra[keys[i]]))
-  }
-  bodyParts.push('--' + boundary + '--')
-
-  const body = bodyParts.join('\r\n')
-
-  return new Promise((resolve, reject) => {
-    fetch.fetch({
-      url: BASE_URL + path,
-      method: 'POST',
-      header: { 'Content-Type': 'multipart/form-data; boundary=' + boundary },
-      data: body,
-      responseType: 'json',
-      success: (res) => {
-        const d = res.data
-        if (d && d.ok) {
-          resolve(d.data)
-        } else {
-          reject({ error: (d && d.error) || '上传失败' })
-        }
-      },
-      fail: (err, code) => reject({ err, code })
+      fail: function (err, code) { reject({ err: err, code: code }) }
     })
   })
 }
 
 export function analyzePhoto(imagePath, mode, uid) {
-  return upload('/api/analyze', imagePath, { mode, uid })
+  return new Promise(function (resolve, reject) {
+    request.upload({
+      url: BASE_URL + '/api/analyze',
+      method: 'POST',
+      header: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data: [
+        { name: 'mode', value: mode },
+        { name: 'uid', value: uid || 'device_unknown' }
+      ],
+      files: [
+        { uri: imagePath, name: 'image', filename: 'photo.jpg' }
+      ],
+      success: function (res) {
+        var d = res.data
+        if (d && d.ok) {
+          resolve(d.data)
+        } else {
+          reject({ error: (d && d.error) || '分析失败' })
+        }
+      },
+      fail: function (err, code) {
+        reject({ err: err, code: code })
+      }
+    })
+  })
 }
 
 export function getUserInfo(uid) {
-  return request('GET', '/user/info?uid=' + uid)
+  return jsonRequest('GET', '/user/info?uid=' + uid)
 }
 
 export function getGallery(uid, page, size) {
   page = page || 1
   size = size || 20
-  return request('GET', '/gallery?uid=' + uid + '&page=' + page + '&size=' + size)
+  return jsonRequest('GET', '/gallery?uid=' + uid + '&page=' + page + '&size=' + size)
 }
 
 export function getGalleryDetail(id) {
-  return request('GET', '/gallery/' + id)
+  return jsonRequest('GET', '/gallery/' + id)
 }
